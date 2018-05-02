af30d331612fa28160c91626d1efd2af427f162e
Clothilde de Villenfagne <clothilde.devillenfagne@student.uclouvain.be>
2018-05-02 19:34:29 +0200

    get height, get width, get a, get b fonction test

--- test.c
+++ test.c
@@ -16,3 +16,27 @@ void testGetAndSetValue(void) {
     CU_ASSERT_EQUAL(x, fractal_get_value(test, 80, 40));
     fractal_free(test);
 }
+
+void testGetHeight(void) {
+    struct fractal * test = fractal_new("fractale", 100, 80, 0.8, -0.8);
+    CU_ASSERT_EQUAL(80, fractal_get_height(test));
+    fractal_free(test);
+}
+
+void testGetWidth(void) {
+    struct fractal * test = fractal_new("fractale", 100, 80, 0.8, -0.8);
+    CU_ASSERT_EQUAL(100, fractal_get_width(test));
+    fractal_free(test);
+}
+
+void testGetA(void) {
+    struct fractal * test = fractal_new("fractale", 100, 50, 0.8, -0.8);
+    CU_ASSERT_EQUAL(0.8, fractal_get_a(test));
+    fractal_free(test);
+}
+
+void testGetB(void) {
+    struct fractal * test = fractal_new("fractale", 100, 50, 0.8, -0.8);
+    CU_ASSERT_EQUAL(-0.8, fractal_get_b(test));
+    fractal_free(test);
+}
