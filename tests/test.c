6cf6ca6b5628418519b0cb84204fdf533f9bfd4a
Clothilde de Villenfagne <clothilde.devillenfagne@student.uclouvain.be>
2018-05-04 13:46:42 +0200

    correction fonction test déjà présente

--- tests/test.c
+++ tests/test.c
@@ -4,13 +4,13 @@
 #include "../stack/stack.h"
 
 void testGetNames(void) {
-    struct fractal * test = fractal_new("fractale", 80, 40, 0.80, -0.8);
+    struct fractal * test = fractal_new(NULL, 80, 40, 0.80, -0.8);
     CU_ASSERT_STRING_EQUAL(name, fractal_get_name(test));
     fractal_free(test);
 }
 
 void testGetAndSetValue(void) {
-    struct fractal * test = fractal_new("fractale", 80, 40, 0.80, -0.8);
+    struct fractal * test = fractal_new("fractal", 80, 40, 0.80, -0.8);
     int x = 20;
     fractal_set_value(test, 80, 40, x);
     CU_ASSERT_EQUAL(x, fractal_get_value(test, 80, 40));
@@ -18,29 +18,36 @@ void testGetAndSetValue(void) {
 }
 
 void testGetHeight(void) {
-    struct fractal * test = fractal_new("fractale", 100, 80, 0.8, -0.8);
+    struct fractal * test = fractal_new("fractal", -100, 80, 0.8, -0.8);
     CU_ASSERT_EQUAL(80, fractal_get_height(test));
     fractal_free(test);
 }
 
 void testGetWidth(void) {
-    struct fractal * test = fractal_new("fractale", 100, 80, 0.8, -0.8);
+    struct fractal * test = fractal_new("fractal", 100, 80, 0.8, -0.8);
     CU_ASSERT_EQUAL(100, fractal_get_width(test));
     fractal_free(test);
 }
 
 void testGetA(void) {
-    struct fractal * test = fractal_new("fractale", 100, 50, 0.8, -0.8);
+    struct fractal * test = fractal_new("fractal", 100, 50, 0.8, -0.8);
     CU_ASSERT_EQUAL(0.8, fractal_get_a(test));
     fractal_free(test);
 }
 
 void testGetB(void) {
-    struct fractal * test = fractal_new("fractale", 100, 50, 0.8, -0.8);
+    struct fractal * test = fractal_new("fractal", 100, 50, 0.8, -0.8);
     CU_ASSERT_EQUAL(-0.8, fractal_get_b(test));
     fractal_free(test);
 }
 
+void testComputeAndGetAverage(void){
+    struct fractal * test = fractal_new("fractal", 100, 50, 0.75, -0.7);
+    fractal_compute_average(test);
+    double moyenne = 2.221400;
+    CU_ASSERT_EQUAL(moyenne,fractal_get_average(test));
+    fractal_free(test);
+}
 
 int main(int argc, char const *argv[]) {
     CU_pSuite pSuite;
@@ -48,19 +55,20 @@ int main(int argc, char const *argv[]) {
         return CU_get_error();
     }
     pSuite = NULL;
-    pSuite = CU_add_suite("Collection de tests sur les fractales", setup, teardown);
+    pSuite = CU_add_suite("Collection de tests sur les fractals", setup, teardown);
     if (pSuite==NULL) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
     if (
-            CU_add_test(pSuite, "test de la librairie fractale, nom", testGetNames)==NULL ||
+            CU_add_test(pSuite, "test sur le nom", testGetNames)==NULL ||
             CU_add_test(pSuite, "test get/set value", testGetAndSetValue)==NULL ||
-            CU_add_test(pSuite, "test de la librairie fractale, largeur", testGetHeight)==NULL ||
-            CU_add_test(pSuite, "test de la librairie fractale, hauteur", testGetWidth)==NULL ||
-            CU_add_test(pSuite, "test de la librairie fractale, a", testGetA)==NULL ||
-            CU_add_test(pSuite, "test de la librairie fractale, b", testGetB)==NULL ){
+            CU_add_test(pSuite, "test sur la largeur", testGetHeight)==NULL ||
+            CU_add_test(pSuite, "test sur la hauteur", testGetWidth)==NULL ||
+            CU_add_test(pSuite, "test sur a", testGetA)==NULL ||
+            CU_add_test(pSuite, "test sur b", testGetB)==NULL ||
+            CU_add_test(pSuite, "test sur compute/get de la moyenne", testComputeAndGetAverage)){
 
         CU_cleanup_registry();
         return CU_get_error();
@@ -71,6 +79,7 @@ int main(int argc, char const *argv[]) {
     CU_basic_show_failures(CU_get_failure_list());
     CU_cleanup_registry();
 
-    printf("\n\n");
+
+    printf("\n THE END \n");
     return 0;
 }
